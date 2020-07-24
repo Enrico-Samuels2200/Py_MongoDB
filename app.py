2a2f2dc0b853f938dd46a0215fe6c1502095cc61
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2020-07-24 09:18:35 +0200

    Made updates to app.py and uploaded app.ipynb
    In app.py I fixed issue(data being duplicated) associated with deleting objects from the collection 'top_three_products'.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["Data_Tracker"]
 product_col = mydb["product_col"]
 
+#   This function calls the requested collection and print it out.
 def display(collection):
     try:
         name = mydb[collection]
@@ -16,7 +17,7 @@ def display(collection):
     except:
         pass
 
-#   Imports data from a JSON file revcieved from SQL database.
+#   Imports data from a JSON file received from SQL database.
 with open("data.json", "r") as line:
     data = json.load(line)
     for i in data: product_col.update(i, i, upsert=True)
@@ -26,9 +27,11 @@ display("product_col")
 
 #   Creates collection for top three products.
 top_products_col = mydb["top_three_products"]
+#---------------------------------------------------------------------------------
+
 
 #   The var 'top_items' selects and returns the first objects from the database.
-#   In line 23 we add the three objects to the collection top_three_products
+#   In line 38 we add the three objects to the collection top_three_products
 top_items = product_col.find().limit(3)
 
 try:
@@ -38,16 +41,21 @@ except:
 
 print(f"\n\n[*] Top Three Products:\n\n")
 for i in product_col.find().limit(3): print(i)
+#---------------------------------------------------------------------------------
+
+
 #   Sorts all the data by their product type in descending order.
 product_col.find().sort("Type", -1)
 print(f"\n\n[*] Descending Data: \n\n")
 display("product_col")
+#---------------------------------------------------------------------------------
 
 
 #   Deletes two products from my top three products.
 top_products_col.delete_many({"Type":"cooldrink"})
 print(f"\n\n[*] Deleted Two Objs:\n\n")
 display('top_three_products')
+#---------------------------------------------------------------------------------
 
 
 #   Updates 1 product and its brands.
@@ -57,26 +65,34 @@ new_brand = {"id": 1, "Type": "cooldrink","Brand/product": "Coke Zero", "Price p
 mydb.product_col.update(item_brand, new_brand, upsert=True)
 print("\n\n[*] Updated Product:\n\n")
 for i in product_col.find({"Brand/product": "Sprite"}): print(i) 
+#---------------------------------------------------------------------------------
+
 
 #   Search for five brands.
 brand_list = ['Simba', 'Monster', 'BarOne', 'Doritos', 'Fanta']
 
-print("[*] Items Searched\n")
+print("\n\n[*] Items Searched\n\n")
 
 item_types = ["chips", "cooldrink", "chocolate", "pies", "cupcakes", "veggies", "fruit", "energy_drink", "sauce"]
 items = {}
 
+#   This loop gathers all objects/dictionaries that contain the item names that are housed within item_types.
+#   This loop then adds values to the dictionary items it add the key value and how many objects there are of
+#   that kind. Example: {'chips':4}
 for i in item_types:
     a = product_col.find({'Type': {'$regex': f'{i}.*'}})
     items[i] = a.count()
 
+#   This sorts the dictionaries values in numerical order.
 data = sorted(items.items(), key=operator.itemgetter(1))
 
+#   Returns the lest 5 types along with all their objects.
 i = 0
 while i < 5:
    b = data[i][0]
    c = product_col.find({"Type":b})
    for item in c: print(item)
    i+= 1
+#---------------------------------------------------------------------------------
 
 myclient.close()
